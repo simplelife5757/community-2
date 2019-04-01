de1e26d388919e2933a7a19da282d72ee6a85c77
Jiwoo Kim <jiwoo627@naver.com>
2019-04-02 03:11:04 +0900

    [INITIAL] FE 웹 프레임워크로 vuejs + vuetifyjs를 구성하였습니다.

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>광일공방</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-btn color="yellow">나야나 버튼</v-btn>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
